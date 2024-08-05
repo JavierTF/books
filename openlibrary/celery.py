8fb3aac8cbcd81609ee678d88f8f404baae10adc
JavierTF <javiertoussentfis@gmail.com>
2024-08-05 02:27:48 -0400

    Celery config

--- openlibrary/celery.py
+++ openlibrary/celery.py
@@ -12,3 +12,17 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 
 # Carga tareas de todos los módulos de tareas registradas en Django apps
 app.autodiscover_tasks()
+
+@app.on_after_configure.connect
+def setup_periodic_tasks(sender, **kwargs):
+    from django_celery_beat.models import PeriodicTask, IntervalSchedule
+
+    schedule, created = IntervalSchedule.objects.get_or_create(
+        every=1,
+        period=IntervalSchedule.DAYS,
+    )
+    PeriodicTask.objects.get_or_create(
+        interval=schedule,
+        name='Fetch books from OpenLibrary daily',
+        task='books.tasks.fetch_books_from_openlibrary',
+    )
