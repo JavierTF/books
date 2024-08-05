fc00bcda863132d8fb29a205b2e6582c24b0f931
JavierTF <javiertoussentfis@gmail.com>
2024-08-04 23:40:08 -0400

    searchs

--- books/views.py
+++ books/views.py
@@ -7,10 +7,59 @@ from django.urls import reverse_lazy
 from .models import Book
 from .forms import BookForm
 
+def search_books(title='', author=''):
+    """
+    Busca libros por título y/o autor usando la API de OpenLibrary.
+    
+    Args:
+        title (str): El título del libro a buscar.
+        author (str): El autor del libro a buscar.
+    
+    Returns:
+        list: Una lista de libros que coinciden con los criterios de búsqueda.
+    """
+    url = "http://openlibrary.org/search.json"
+    params = {}
+    
+    if title:
+        params['title'] = title
+    if author:
+        params['author'] = author
+    
+    response = requests.get(url, params=params)
+    
+    if response.status_code == 200:
+        books = response.json().get('docs', [])
+        return books
+    else:
+        print(f"Error: Unable to fetch data, received status code {response.status_code}")
+        return []
+
+def book_search_view(request):
+    """
+    Vista de Django para buscar libros por título y/o autor.
+    """
+    title = request.GET.get('title', '')
+    author = request.GET.get('author', '')
+    books = search_books(title=title, author=author)
+    return JsonResponse(books, safe=False)
+
 class BookListView(ListView):
     model = Book
     template_name = 'books/book_list.html'
     context_object_name = 'books'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        title_query = self.request.GET.get('title')
+        author_query = self.request.GET.get('author')
+
+        if title_query:
+            queryset = queryset.filter(title__icontains=title_query)
+        if author_query:
+            queryset = queryset.filter(author__icontains=author_query)
+            
+        return queryset
 
 class AddBookView(CreateView):
     model = Book
@@ -41,33 +90,3 @@ class DeleteBookView(DeleteView):
     model = Book
     template_name = 'books/book_confirm_delete.html'
     success_url = reverse_lazy('books:book_list')
-
-def search_books_by_title(title):
-    """
-    Busca libros por título usando la API de OpenLibrary.
-    
-    Args:
-        title (str): El título del libro a buscar.
-    
-    Returns:
-        list: Una lista de libros que coinciden con el título.
-    """
-    url = "http://openlibrary.org/search.json"
-    params = {'title': title}
-    
-    response = requests.get(url, params=params)
-    
-    if response.status_code == 200:
-        books = response.json().get('docs', [])
-        return books
-    else:
-        print(f"Error: Unable to fetch data, received status code {response.status_code}")
-        return []
-
-def book_search_view(request):
-    """
-    Vista de Django para buscar libros por título.
-    """
-    title = request.GET.get('title', '')
-    books = search_books_by_title(title)
-    return JsonResponse(books, safe=False)
